d7ccc5f8213ecdf6b0431072f6a2dc1039bfd248
dmendivelso <danmen2001@gmail.com>
2020-11-10 16:25:04 -0500

    costo

---
+++ src/main/java/com/vetun/apirest/repository/CostoRepository.java
@@ -0,0 +1,7 @@
+package com.vetun.apirest.repository;
+
+import com.vetun.apirest.model.Costo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CostoRepository extends JpaRepository<Costo, Integer> {
+}
---
+++ src/main/java/com/vetun/apirest/service/CostoService.java
@@ -0,0 +1,22 @@
+package com.vetun.apirest.service;
+
+import com.vetun.apirest.model.Cita;
+import com.vetun.apirest.model.Costo;
+import com.vetun.apirest.repository.CostoRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CostoService {
+
+    private CostoRepository costoRepository;
+
+
+    public Costo findById(Integer id ){
+        return  costoRepository.findById( id ).orElse( null );
+    }
+
+    public void save(Costo costo){
+        costoRepository.save(costo);
+    }
+
+}

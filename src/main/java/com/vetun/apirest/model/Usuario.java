4b94cf8f4ad51cfb172f9c46e321f16c83259bfc
dmendivelso <danmen2001@gmail.com>
2021-07-02 20:59:22 -0500

    2FA

--- src/main/java/com/vetun/apirest/model/Usuario.java
+++ src/main/java/com/vetun/apirest/model/Usuario.java
@@ -6,43 +6,49 @@ import javax.persistence.*;
 
 
 @Entity
-@Table(name="usuario")
+@Table(name = "usuario")
 public class Usuario {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
-    @Column(name="id_usuario")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_usuario")
     private int idUsuario;
 
     @JsonIgnore
-    @OneToOne(fetch = FetchType.LAZY, cascade =  CascadeType.ALL, mappedBy = "usuario")
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "usuario")
     private Dueno dueno;
 
     @JsonIgnore
-    @OneToOne(fetch = FetchType.LAZY, cascade =  CascadeType.ALL, mappedBy = "usuario")
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "usuario")
     private Medico medico;
 
     @ManyToOne
-    @JoinColumn(name = "id_rol" )
+    @JoinColumn(name = "id_rol")
     private Rol rol;
 
-    @JoinColumn(name = "username" )
+    @JoinColumn(name = "username")
     private String username;
 
     @JsonIgnore
-    @JoinColumn(name = "password" )
+    @JoinColumn(name = "password")
     private String password;
 
-    @JoinColumn(name = "correo_electronico" )
+    @JoinColumn(name = "correo_electronico")
     private String correoElectronico;
 
     @JsonIgnore
-    @OneToOne(fetch = FetchType.LAZY, cascade =  CascadeType.ALL, mappedBy = "usuario")
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "usuario")
     private PasswordReset reset;
 
+    @JoinColumn(name = "token_2fa")
+    private String token2FA;
+
+    @JoinColumn(name = "active_2fa")
+    private Boolean active2FA;
 
     public Usuario() {
-        //Necesary method
+        this.token2FA = null;
+        this.active2FA = false;
     }
 
     public Usuario(int idUsuario, Dueno dueno, Medico medico, Rol rol, String username, String password, String correoElectronico) {
@@ -53,6 +59,8 @@ public class Usuario {
         this.username = username;
         this.password = password;
         this.correoElectronico = correoElectronico;
+        this.token2FA = null;
+        this.active2FA = false;
     }
 
     public int getIdUsuario() {
@@ -109,6 +117,22 @@ public class Usuario {
 
     public void setCorreoElectronico(String correoElectronico) {
         this.correoElectronico = correoElectronico;
+    }
+
+    public String getToken2FA() {
+        return token2FA;
+    }
+
+    public void setToken2FA(String token2FA) {
+        this.token2FA = token2FA;
+    }
+
+    public Boolean getActive2FA() {
+        return active2FA;
+    }
+
+    public void setActive2FA(Boolean active2FA) {
+        this.active2FA = active2FA;
     }
 
     @Override
